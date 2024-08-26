347c880b7383c73538670fbe6aafaf1df2436e36
declan <declanstambaugh@gmail.com>
2024-08-26 16:10:39 -0600

    small sayhi fix

--- scripts/functions.js
+++ scripts/functions.js
@@ -2,7 +2,8 @@
 let sayhi = function() {
     let firstName = "Declan";
     let age = 13;
-    let greeting = "Hello! my name is" + firstName + ", and I am" + age + "years old";
+    let greeting = 
+        "Hello! my name is" + firstName + ", and I am" + age + "years old";
     console.log(greeting);
 };
 
