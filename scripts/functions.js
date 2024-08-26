2fbf11b1163587ea643349bd8a64641f2aaa0d57
declan <declanstambaugh@gmail.com>
2024-08-26 16:09:12 -0600

    say hi function

---
+++ scripts/functions.js
@@ -0,0 +1,9 @@
+//@ts-check
+let sayhi = function() {
+    let firstName = "Declan";
+    let age = 13;
+    let greeting = "Hello! my name is" + firstName + ", and I am" + age + "years old";
+    console.log(greeting);
+};
+
+sayhi()
