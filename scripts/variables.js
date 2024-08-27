591a7342776b32fef0d487c644536d416fbc8d9a
declan <declanstambaugh@gmail.com>
2024-08-26 21:01:21 -0600

    New Variables file

---
+++ scripts/variables.js
@@ -0,0 +1,9 @@
+// this varible is a number or numerical data
+let intelligenceQuotion = 8;
+
+let firstName = "Declan ";
+let lastName = "Stambaugh";
+
+firstName = "John";
+
+console.log(firstName, lastName, "has IQ", intelligenceQuotion)
