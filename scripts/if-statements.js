96c19071457c9c429ebf20677586cbd5d7124c0e
declan <declanstambaugh@gmail.com>
2024-09-03 16:25:08 -0600

    everything is working now

---
+++ scripts/if-statements.js
@@ -0,0 +1,30 @@
+//@ts-check
+
+let iq = 150
+
+if (iq) {
+    console.log("Let into Ivy leauge")
+} else {
+    console.log("Your rejected by society")
+}
+
+
+let isTheSkyBlue = false
+let isTheSunUp = false
+
+
+if (isTheSkyBlue)  {
+//this will run if true
+let grassColor = "Green"
+    console.log("The Sky is indeed blue");
+    console.log(grassColor);
+} else if (!isTheSunUp) {
+    let grassColor = "Green"
+    console.log("It must be night");
+    grassColor = "Dark green"
+    console.log(grassColor);
+}else {
+//this will run if false
+    console.log("It must be cloudy or a solar eclipse");
+}
+
