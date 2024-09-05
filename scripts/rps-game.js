32f3e90ce739c0d7d163bccdb62d0a8ed381fd37
declan <declanstambaugh@gmail.com>
2024-09-05 15:40:59 -0600

    game

--- scripts/rps-game.js
+++ scripts/rps-game.js
@@ -1,9 +1,30 @@
 //@ts-check
 
+let playerControls = document.getElementById("player-controls");
+
+let btnRock = document.getElementById("btn-rock");
+
+let btnPaper = document.getElementById("btn-paper");
+
+let btnScissors = document.getElementById("btn-scissors");
 
 
+function rockClicked(rock) {
+    console.log(rock, " was selected");
+}
+
+btnRock?.addEventListener("click", rockClicked)
 
 
+function paperClicked(paper) {
+    console.log(paper, " was selected");
+}
+
+btnPaper?.addEventListener("click", paperClicked)
 
 
+function scissorsClicked(scissors) {
+    console.log(scissors, " was selected");
+}
 
+btnScissors?.addEventListener("click", scissorsClicked)
