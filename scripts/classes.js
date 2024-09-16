70aa054ee9414fc2540f9b36fe82fa72bf8203b5
declan <declanstambaugh@gmail.com>
2024-09-16 16:21:26 -0600

    cheese and classes

---
+++ scripts/classes.js
@@ -0,0 +1,25 @@
+//@ts-check
+
+class CheeseClass {
+    constructor(type,) {
+        this.type = type;
+        this.age = 0;
+        this.shape = "wheel";
+        this.color = "yellow";
+        this.cost = 0.0;
+    }
+
+    eat() {
+        console.log (`Nom nom nom ${this.type} is good`)
+    }
+}
+
+let cheddar = new CheeseClass("Cheddar");
+cheddar.shape = "fish";
+cheddar.eat
+let swiss = new CheeseClass("Swiss");
+swiss.color = "white";
+swiss.eat
+let gouda = new CheeseClass("gouda");
+gouda.age = 500;
+gouda.eat
